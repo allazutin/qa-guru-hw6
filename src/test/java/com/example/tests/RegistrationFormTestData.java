f62b7fa41f15a6b8b0858fdacd0bd02047964654
Alexander Lazutin <alazutin@qahv1.veeam.local>
2022-08-14 20:48:52 +0300

    fixed

--- src/test/java/com/example/tests/RegistrationFormTestData.java
+++ src/test/java/com/example/tests/RegistrationFormTestData.java
@@ -1,2 +1,26 @@
-package com.example.tests;public class RegistrationFormTestData {
+package com.example.tests;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+
+public class RegistrationFormTestData {
+    static Faker enFakeData = new Faker(new Locale("en-US"));
+
+    static String firstname = enFakeData.name().firstName(),
+            lastname = enFakeData.name().lastName(),
+            email = enFakeData.internet().emailAddress(),
+            gender = "Male",
+            phonenumber = enFakeData.number().digits(10),
+            day = "15",
+            month = "July",
+            year = "2000",
+            subject = "Math",
+            uploadPicture = "img.jpg",
+            hobbies = "Sports",
+            address = enFakeData.address().fullAddress(),
+            state = "Uttar Pradesh",
+            city = "Agra";
 }
--- src/test/java/com/example/tests/RegistrationFormTests.java
+++ src/test/java/com/example/tests/RegistrationFormTests.java
@@ -1,14 +1,12 @@
 package com.example.tests;
 
-import com.example.pages.RegistrationFormPage;
 import com.codeborne.selenide.Configuration;
+import com.example.pages.RegistrationFormPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.example.tests.RegistrationFormTestData.*;
+import static java.lang.String.format;
 
 public class RegistrationFormTests {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
@@ -21,31 +19,36 @@ public class RegistrationFormTests {
     @Test
     void fillFormTest() {
         registrationFormPage.openPage()
-                .setFirstName("Ivan")
-                .setLastName("Ivanov")
-                .setEmail("ivan.ivanov@example.com")
-                .setGender("Male")
-                .setNumber("1234567890")
-                .setBirthDate("01", "January", "2000")
-                .setSubjects("Math")
-                .uploadPicture("img.jpg")
-                .setHobbies("Sports")
-                .setAddress("Main st. 666")
-                .setState("Uttar Pradesh")
-                .setCity("Agra")
+                .setFirstName(firstname)
+                .setLastName(lastname)
+                .setEmail(email)
+                .setGender(gender)
+                .setNumber(phonenumber)
+                .setBirthDate(day, month, year)
+                .setSubjects(subject)
+                .uploadPicture(uploadPicture)
+                .setHobbies(hobbies)
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
                 .submit();
 
+        String expectedFullName = format("%s %s", firstname, lastname),
+         expectedDate = format("%s %s,%s", day, month, year),
+                expectedStateAndCity = format("%s %s", state, city);
+
         registrationFormPage.checkResultsTableVisible()
-                .checkResult("Student Name", "Ivan Ivanov")
-                .checkResult("Student Email", "ivan.ivanov@example.com")
-                .checkResult("Gender","Male")
-                .checkResult("Mobile","1234567890")
-                .checkResult("Date of Birth", "01 January,2000")
-                .checkResult("Subjects", "Math")
-                .checkResult("Hobbies", "Sports")
-                .checkResult("Picture", "img.jpg")
-                .checkResult("Address", "Main st. 666")
-                .checkResult("State and City", "Uttar Pradesh Agra");
+
+                .checkResult("Student Name", expectedFullName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender",gender)
+                .checkResult("Mobile",phonenumber)
+                .checkResult("Date of Birth", expectedDate)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobbies)
+                .checkResult("Picture", uploadPicture)
+                .checkResult("Address", address)
+                .checkResult("State and City", expectedStateAndCity);
     }
 }
 
